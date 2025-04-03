4e157972ab9327208423e73e988f9d57117ee93e
maksym93872747823 <diadia.maxsim@gmail.com>
2025-04-03 19:00:04 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,14 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 st.markdown(
     """
     <h1 style='text-align: center; color: #F9FAFB;'>📚 <span style='color:#00BFFF'>Можливо все</span></h1>
-    <p style='text-align: center; font-size:18px; color: #D1D5DB;'>
+    <p style='text-align: center; font-size:18px; color: #000000; background-color: rgba(0, 0, 0, 0.5); padding: 10px; border-radius: 8px;'>
         Цитати, ідеї та натхнення, зібрані нашою командою під час аналізу книги.
     </p>
     <hr style='border: 1px solid #444;'/>
     """,
     unsafe_allow_html=True
 )
+
 
 # Подключение Google Таблицы
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3cQlzWgr-dv_MC_usm7D2Lr2-XGG7HosOcMvLMQF3_e672gdHaTo8jxpJ77fwrPrwjKNyRh53IjLT/pub?output=csv"
