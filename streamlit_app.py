a948fe6cb850514fb0c4a539d5913298c59c9372
maksym93872747823 <diadia.maxsim@gmail.com>
2025-04-03 17:32:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,19 @@ st.markdown(
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3cQlzWgr-dv_MC_usm7D2Lr2-XGG7HosOcMvLMQF3_e672gdHaTo8jxpJ77fwrPrwjKNyRh53IjLT/pub?output=csv"
 df = pd.read_csv(url)
 
-# Выводим таблицу на сайт
-st.subheader("Актуальні дані з таблиці")
+# Фільтр по авторам
+if 'Автор' in df.columns:
+    selected_author = st.selectbox("Обрати автора", options=["Всі"] + sorted(df['Автор'].dropna().unique()))
+    if selected_author != "Всі":
+        df = df[df['Автор'] == selected_author]
+
+# Фільтр по главах
+if 'Глава' in df.columns:
+    selected_chapter = st.selectbox("Обрати главу", options=["Всі"] + sorted(df['Глава'].dropna().unique()))
+    if selected_chapter != "Всі":
+        df = df[df['Глава'] == selected_chapter]
+
+# Вивід таблиці після фільтрації
+st.subheader("Результати після фільтрації")
 st.dataframe(df)
 
