457b73a52bf164cb1376f188e4e9615b0753fe9f
maksym93872747823 <diadia.maxsim@gmail.com>
2025-04-03 18:55:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import pandas as pd
-# Фонове зображення 
+
+# Фоновое изображение
 page_bg_img = '''
 <style>
-body {
+.stApp {
     background-image: url("https://raw.githubusercontent.com/maksym93872747823/streamlit_project_group17/main/mountain_bg.jpg");
     background-size: cover;
     background-repeat: no-repeat;
     background-attachment: fixed;
     background-position: center;
-    height: 100%;
+    min-height: 100vh; /* Покрывает весь экран */
 }
 </style>
 '''
 st.markdown(page_bg_img, unsafe_allow_html=True)
-
 
 # Заголовок
 st.markdown(
@@ -32,20 +32,17 @@ st.markdown(
 url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3cQlzWgr-dv_MC_usm7D2Lr2-XGG7HosOcMvLMQF3_e672gdHaTo8jxpJ77fwrPrwjKNyRh53IjLT/pub?output=csv"
 df = pd.read_csv(url)
 
-
-
-# Фільтр по імені (Автор)
+# Фільтри
 if "Ім'я" in df.columns:
     selected_author = st.selectbox("Обрати учасника", ["Всі"] + sorted(df["Ім'я"].dropna().unique()))
     if selected_author != "Всі":
         df = df[df["Ім'я"] == selected_author]
 
-# Фільтр по назві розділу (Глава)
 if "Назва розділу" in df.columns:
     selected_chapter = st.selectbox("Обрати розділ", ["Всі"] + sorted(df["Назва розділу"].dropna().unique()))
     if selected_chapter != "Всі":
         df = df[df["Назва розділу"] == selected_chapter]
 
-# Вивід таблиці після фільтрації
+# Виведення таблиці після фільтрації
 st.subheader("Результати після фільтрації")
 st.dataframe(df)
